d10e5ec1cab23549392e3ec42ddb65267c722914
KhaliD HassaN <khalid.hassan1612@gmail.com>
2019-02-05 21:53:25 +0600

    init commit

---
+++ app/src/main/java/com/example/unitconverter/PowerConvert.java
@@ -0,0 +1,27 @@
+package com.example.unitconverter;
+
+public class PowerConvert {
+    public static double wattToKilowatt(double watt){
+        return watt / 1000;
+    }
+
+    public static double wattToHorsepower(double watt){
+        return watt * .001341;
+    }
+
+    public static double kilowattToWatt(double kilowatt){
+        return kilowatt * 1000;
+    }
+
+    public static double kilowattToHorsepower(double kilowatt){
+    return kilowatt * 1.341022;
+    }
+
+    public static double horsepowerToWatt(double horsepower){
+        return horsepower / .001341;
+    }
+
+    public static double horsepowerToKilowatt(double horsepower){
+        return horsepower / 1.341022;
+    }
+}
